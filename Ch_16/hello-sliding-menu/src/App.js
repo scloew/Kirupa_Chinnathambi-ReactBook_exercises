b2b13a710f195fd987d3c6a33bcee0a88ac5548d
scloew <stevecloew@gmail.com>
2020-02-23 09:48:05 -0600

    started ch 16
    set up initial project but have yet to get into the sliding bar part

---
+++ Ch_16/hello-sliding-menu/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MenuContainer from './MenuContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <MenuContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Ch_16/hello-sliding-menu/src/MenuContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class MenuContainer extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <p> can you spot the item that doesn't belong?</p>
+          <li>na</li>
+          <li>na</li>
+          <li>nana</li>
+          <li>Iron Man!</li>
+          <li>DO'H</li>
+        </div>
+      </div>
+    );
+  }
+}
