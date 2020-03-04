6a9d2eeb84e4804c05073492f0e631425dfab6ab
scloew <stevecloew@gmail.com>
2020-03-03 21:11:30 -0600

    finished ch 17
    learned about the shouldComponentUpdate method and PureComponent buT REALLY SHOULDN'T need these hardly ever

--- Ch_17/hello-sliding-menu/src/MenuButton.js
+++ Ch_17/hello-sliding-menu/src/MenuButton.js
@@ -1,7 +1,22 @@
-import React, { Component } from 'react';
+/**
+ * Comments denoted to make MenuButton a PureComponent
+ * this avoids manually having to check if the components
+ * render method needs to be called. Pretty neat-o-burrito
+ */
+
+//import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import './MenuButton.css';
 
-export default class MenuButton extends Component {
+//export default class MenuButton extends Component {
+export default class MenuButton extends PureComponent {
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return nextProps.handleMouseDown === this.props.handleMouseDown ?
+  //     false : true;
+  // }
+  //Above code isn't necessary with pure code
+
   render() {
     console.log('rendering: menu button');
     return (
